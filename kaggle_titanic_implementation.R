79124356621cff5ab84e35c72b70e19ae8e37dcf
Audrey Batzel <abatzel@ihme.washington.edu>
2019-09-02 16:10:24 -0700

    feature engineering and initial model runs

--- kaggle_titanic_implementation.R
+++ kaggle_titanic_implementation.R
@@ -34,8 +34,43 @@ data[, title := gsub('(.*, )|(\\..*)', '', Name)]
 data[, Surname := sapply(Name, function(x) strsplit(x, split = '[,.]')[[1]][1])]
 
 
+md.pattern(data)
 
 
+# Set a random seed
+set.seed(754)
+set.seed(700)
+# Make variables factors into factors
+# factor_vars <- c('title','Sex')
+# 
+# data[factor_vars] = lapply(data[factor_vars], function(x) as.factor(x))
 
+data[, Sex := as.factor(Sex)]
+data[, title := as.factor(title)]
 
+# Build the model (note: not all possible variables are used)
+rf_model = randomForest(factor(Survived) ~ Pclass + Sex + title,
+                         data = data)
 
+# Show model error
+plot(rf_model, ylim=c(0,0.36))
+legend('topright', colnames(rf_model$err.rate), col=1:3, fill=1:3)
+
+# Get importance
+importance    <- importance(rf_model)
+varImportance <- data.frame(Variables = row.names(importance), 
+                            Importance = round(importance[ ,'MeanDecreaseGini'],2))
+
+# Create a rank variable based on importance
+rankImportance <- varImportance %>%
+  mutate(Rank = paste0('#',dense_rank(desc(Importance))))
+
+# Use ggplot2 to visualize the relative importance of variables
+ggplot(rankImportance, aes(x = reorder(Variables, Importance), 
+                           y = Importance, fill = Importance)) +
+  geom_bar(stat='identity') + 
+  geom_text(aes(x = Variables, y = 0.5, label = Rank),
+            hjust=0, vjust=0.55, size = 4, colour = 'red') +
+  labs(x = 'Variables') +
+  coord_flip() + 
+  theme_bw()
